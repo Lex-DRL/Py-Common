940e4cd269bd0758c304135c1721a8ae33ec7a47
DRL <drl.aivik@gmail.com>
2019-04-30 23:26:19 +0300

    initial geo_math module

---
+++ drl_common/geo_math.py
@@ -0,0 +1,14 @@
+"""
+Abstract geometry math, not tied specifically to any 3D software.
+"""
+
+__author__ = 'Lex Darlog (DRL)'
+
+try:
+	# support type hints in Python 3:
+	from typing import *
+except ImportError:
+	pass
+from drl_common.py_2_3 import str_t
+
+# TODO: https://gist.github.com/RedForty/9fc37bc8ad647256177c0749065ac262
