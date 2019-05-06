b14cb75b476b261251db38ed4f315a463b3f8be2
DRL <drl.aivik@gmail.com>
2019-05-06 22:23:23 +0300

    points_closest_plane(): initial cleanup, separating it from Maya

--- drl_common/geo_math.py
+++ drl_common/geo_math.py
@@ -4,40 +4,123 @@ Abstract geometry math, not tied specifically to any 3D software.
 
 __author__ = 'Lex Darlog (DRL)'
 
+from .utils import (
+	flatten_gen as _flatten,
+	_Iterable,
+	_Iterator
+)
+
 try:
 	# support type hints in Python 3:
 	from typing import *
 except ImportError:
 	pass
-from drl_common.py_2_3 import str_t
-
-# TODO: https://gist.github.com/RedForty/9fc37bc8ad647256177c0749065ac262
-import maya.cmds as cmds
-import maya.api.OpenMaya as api
+from drl_common.py_2_3 import str_t, str_hint
 
 
-def bestFitPlane(points, plane=False):
+def points_closest_plane(
+	positions  # type: Sequence[api.MVector]
+):
 	"""
+	https://gist.github.com/RedForty/9fc37bc8ad647256177c0749065ac262
+
 	Takes an array of points (transforms)
 	Returns a plane located at the centroid of the points
 	It is oriented as a best fit plane using this method:
 	http://www.ilikebigbits.com/blog/2017/9/24/fitting-a-plane-to-noisy-points-in-3d
 	Optionally, you can feed it a plane and it will merely position and orient it
 	"""
-	if not points:  # Validate input
+	if not positions:  # Validate input
 		return None
 
-	pointPositions = {}
-	for point in points:
-		vector = api.MVector(cmds.xform(point, q=True, t=True, ws=True))
-		pointPositions[point] = vector
+	from .is_maya import is_maya as _is_maya
+	is_maya = _is_maya()
 
-	pointCount = 0
+	if is_maya:
+		from maya import cmds
+		from maya.api import OpenMaya as api
+		from pymel import core as pm
+
+		from drl.for_maya.ls.convert import components as comp
+
+	def slice_xyz(
+		iterable  # type: Iterable
+	):
+		"""
+		Generator that always produces a sequence of exactly 3 float/int elements.
+		It's kinda a slice, but works even if the original iterable
+		doesn't support slicing, and also fills missing components with zeroes.
+		"""
+		i = 0  # after the loop, it will have the num of sent components
+		# loop over given components, but only up to the 3rd:
+		for comp in iterable:
+			if not isinstance(comp, (int, float)):
+				continue
+			if i > 2:
+				break
+			yield comp
+			i += 1
+		# if we sent less then 3, fill the remaining ones with zero:
+		for x in xrange(3 - i):
+			yield 0
+
+	def cleanup_pos_as_maya(iterable):
+		"""
+		A generator that turns whatever input is given
+		to an iterable of MVector items.
+
+		Supported inputs are:
+			* the actual MVectors
+			* components given either as PyNode objects or as a cmds's style strings.
+
+		Anything else is simply filtered out.
+		"""
+		def _process_comps(comps):
+			"""
+			A generator, converting a given comp(s) to vertices
+			and then reading their positions.
+			"""
+			comps = (c for c in comps if isinstance(c, pm.Component))
+			verts = set(
+				comp.Poly(comps, selection_if_none=False).to_vertices(flatten=True)
+			)
+			for v in verts:
+				pos_comps = pm.xform(v, q=True, t=True, ws=True)  # type: List[float]
+				# noinspection PyArgumentList
+				yield api.MVector(pos_comps)
+
+		# we may have a single item, which corresponds to multiple vertices:
+		if isinstance(iterable, str_t) or isinstance(iterable, pm.Component):
+			for vector in _process_comps(pm.ls(iterable)):
+				yield vector
+				return
+
+		if not isinstance(iterable, (_Iterable, _Iterator)):
+			# we've got a single item and it can't represent multiple points
+			return
+
+		for pos in iterable:
+			if isinstance(pos, api.MVector):
+				yield pos
+				continue
+
+			if isinstance(pos, str_t) or isinstance(pos, pm.Component):
+				for vector in _process_comps(pm.ls(pos)):
+					yield vector
+				continue
+
+			if isinstance(pos, (_Iterable, _Iterator)):
+				px, py, pz = slice_xyz(pos)
+				# noinspection PyArgumentList
+				yield api.MVector(px, py, pz)
+
+	positions = list(positions)
+
+	num_p = len(positions)
 	averagePosition = api.MVector()
-	for point in pointPositions.values():
-		pointCount += 1
+	for point in positions:
 		averagePosition += point
-	centroid = averagePosition / pointCount
+	centroid = averagePosition / num_p
 
 	# loc = 'derp'
 	# loc = cmds.spaceLocator(n='derp', a=True)
@@ -51,7 +134,7 @@ def bestFitPlane(points, plane=False):
 	yz = 0.0
 	zz = 0.0
 
-	for point in pointPositions.values():
+	for point in positions:
 		r = point - centroid
 		xx += r.x * r.x
 		xy += r.x * r.y
@@ -60,12 +143,12 @@ def bestFitPlane(points, plane=False):
 		yz += r.y * r.z
 		zz += r.z * r.z
 
-	xx /= pointCount
-	xy /= pointCount
-	xz /= pointCount
-	yy /= pointCount
-	yz /= pointCount
-	zz /= pointCount
+	xx /= num_p
+	xy /= num_p
+	xz /= num_p
+	yy /= num_p
+	yz /= num_p
+	zz /= num_p
 
 	weighted_dir = api.MVector()
 
@@ -99,10 +182,3 @@ def bestFitPlane(points, plane=False):
 	plane = cmds.polyPlane(name='bestFitPlane', width=10, height=10, sx=1, sy=1)
 	cmds.xform(plane, ro=angle, t=centroid)
 	return plane
-
-
-# TO DO - Figure out how to flip the normal to face a specified direction. Ex. If the bone is on the other side.
-
-# Select a bunch of locators/transforms
-sel = cmds.ls(sl=1, fl=1)
-bestFitPlane(sel)
