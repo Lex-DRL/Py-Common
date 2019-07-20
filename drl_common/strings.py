5879bffb8842fe63ca7ee76e0a0de470ff44e4f8
DRL <drl.aivik@gmail.com>
2019-07-20 03:07:31 +0300

    strings: list_with_and()

--- drl_common/strings.py
+++ drl_common/strings.py
@@ -2,6 +2,7 @@ from py_2_3 import (
 	typing as _t,
 	str_t, str_hint, izip
 )
+import collections as _c
 
 __author__ = 'DRL'
 
@@ -58,3 +59,43 @@ def error_if_not_str(var, var_name="variable"):
 		raise Exception(
 			'The {0} needs to be a string or unicode. Got: {1}'.format(var_name, repr(var))
 		)
+
+
+def to_str(item):
+	"""
+	Service function which turns everything to a printable string.
+
+	The string/unicode object stays as is, anything else is turned to a string
+	with repr().
+	"""
+	return item if isinstance(item, str_t) else repr(item)
+
+
+def list_with_and(
+	iterable,  # type: _t.Iterable
+	and_sep=' and '  # type: str_hint
+):
+	"""
+	The alternative to ', '.join(iterable), which generates a more human-like
+	sequence. I.e., the last element is attached with an "and" separator.
+	"""
+	if not iterable:
+		return ''
+
+	if not isinstance(iterable, _c.Sequence):
+		iterable = list(iterable)
+	try:
+		last = iterable[-1]
+	except IndexError:
+		return ''
+
+	iterable = iterable[:-1]
+	try:
+		prefix = ', '.join(to_str(x) for x in iterable)
+	except UnicodeError:
+		prefix = u', '.join(to_str(x) for x in iterable)
+
+	if not prefix:
+		return last
+
+	return prefix + and_sep + last
