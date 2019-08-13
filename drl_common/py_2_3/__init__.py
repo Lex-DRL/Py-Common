66c5147b8c9bdec5c93303e070f0b2c83896442b
DRL-aivik <drl.aivik@gmail.com>
2019-08-13 19:48:06 +0300

    py_2_3: enum

--- drl_common/py_2_3/__init__.py
+++ drl_common/py_2_3/__init__.py
@@ -57,3 +57,76 @@ except:
 # 	str_hint = typing.Union[str_hint, typing.AnyStr]
 # except:
 # 	pass
+
+
+def __pip_inst(
+	module_names,  # type: typing.Union[str_hint, typing.Iterable[str_hint]]
+	upgrade=True,
+	force_reinstall=False
+):
+	"""
+	A simplified version of ``pip_install``, to avoid recursive dependencies.
+	"""
+	if isinstance(module_names, str_t):
+		module_names = module_names.split()
+	module_names = filter(None, module_names)
+
+	pip_args = [
+		arg for arg, do_arg in (
+			('install', True),
+			('--upgrade', upgrade),
+			('--force-reinstall', force_reinstall),
+		) if do_arg
+	]
+	pip_args += module_names
+
+	try:
+		import pip
+		# noinspection PyUnresolvedReferences
+		main_f = pip.main
+	except (AttributeError, ImportError):
+		# noinspection PyProtectedMember
+		from pip import _internal as pip_internal
+		main_f = pip_internal.main
+	main_f(pip_args)
+
+
+try:
+	try:
+		import enum
+	except (AttributeError, ImportError):
+		try:
+			__pip_inst('enum34', True, False)
+			import enum
+		except ImportError:
+			__pip_inst(['pip', 'enum34'], True, True)
+			import enum
+except BaseException as e:
+	try:
+		from os import path as __pa
+		import sys as __sys
+
+		__enum_path = __pa.join(
+			__pa.dirname(__pa.abspath(__file__)),
+			'__local_cache',
+			(
+				'enum34-1.1.6-py3-none-any.whl'
+				if __sys.version_info[0] > 2
+				else 'enum34-1.1.6-py2-none-any.whl'
+			)
+		)
+		__pip_inst([__enum_path], True, True)
+		import enum
+
+		print (
+			'Unable to import a built-in <enum> module or download it '
+			'from the internet due to the error: {}\n'
+			'Installing a locally cached version: {}.'.format(e, __enum_path)
+		)
+	except BaseException as e2:
+		from .__local_fallback import enum
+		print (
+			'Unable to import a built-in <enum> module '
+			'or install it from a local cache due to the errors: {}\n{}\n'
+			'Using locally stored copy instead.'.format(e, e2)
+		)
