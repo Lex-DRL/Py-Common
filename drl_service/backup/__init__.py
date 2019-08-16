ffebd85c342528f5938a7c3d51dae109189c273b
DRL-aivik <drl.aivik@gmail.com>
2019-08-16 22:31:18 +0300

    backup.error_types enum-module -> BackupErrorType Enum-class

--- drl_service/backup/__init__.py
+++ drl_service/backup/__init__.py
@@ -18,9 +18,47 @@ from os import path as _path
 import errno as _errno
 
 from drl_os import process as _prc
-from . import modes, error_types
+from drl_common.py_2_3.enum import EnumDefault as __EnumDefault
+from . import modes
 
-_errno_all = set(_errno.errorcode.itervalues())
+_errno_all = frozenset(_errno.errorcode.itervalues())
+
+
+class BackupErrorType(__EnumDefault):
+	"""Supported modes for ``BackupError``."""
+
+	# default:
+	UNKNOWN = 0  # type: BackupErrorType
+
+	SRC_NOT_EXIST = 11  # type: BackupErrorType
+	SRC_IS_FILE = 12  # type: BackupErrorType
+	SRC_IS_DIR = 13  # type: BackupErrorType
+
+	OUT_NOT_EXIST = 21  # type: BackupErrorType
+	OUT_IS_FILE = 22  # type: BackupErrorType
+	OUT_IS_DIR = 23  # type: BackupErrorType
+	OUT_CANT_DELETE = 23  # type: BackupErrorType
+
+
+__type_errno_map = {
+	BackupErrorType.UNKNOWN: _errno.EPERM,
+
+	BackupErrorType.SRC_NOT_EXIST: _errno.ENOENT,
+	BackupErrorType.SRC_IS_FILE: _errno.ENOTDIR,
+	BackupErrorType.SRC_IS_DIR: _errno.EISDIR,
+
+	BackupErrorType.OUT_NOT_EXIST: _errno.ENOENT,
+	BackupErrorType.OUT_IS_FILE: _errno.ENOTDIR,
+	BackupErrorType.OUT_IS_DIR: _errno.EISDIR,
+	BackupErrorType.OUT_CANT_DELETE: _errno.EBUSY,
+}
+
+
+def error_type_errno(
+	error_type  # type: _t.Union[BackupErrorType, int]
+):
+	"""Get a corresponding errno for the given error-type."""
+	return __type_errno_map[BackupErrorType.get(error_type)]
 
 
 class BackupError(EnvironmentError):
@@ -32,31 +70,33 @@ class BackupError(EnvironmentError):
 	def __init__(
 		self,
 		message='',  # type: _str_hint
-		type=None,  # type: _t.Optional[int]
+		type=None,  # type: _t.Optional[_t.Union[BackupErrorType, int]]
 		path=None,  # type: _t.Optional[_str_hint]
 		errno=None  # type: _t.Optional[int]
 	):
 		"""
 		:param message: The string passed as a `strerror` to the OS.
-		:param type: The sub-type of an error. See `error_types` submodule.
+		:param type:
+			The sub-type of an error. See ``BackupErrorType`` Enum.
+
+			If omitted, the default **UNKNOWN** ``BackupErrorType`` is used.
 		:param path: The path of a file/dir causing the error.
 		:param errno:
 			You can explicitly specify the error-number
-			(from the built-in `errno` module).
+			(from the built-in ``errno`` module).
 			Otherwise, it will be automatically detected from the error type
-			with `error_types.type_errno()`, but it may be not the best match for
+			with ``error_type_errno()``, but it may be not the best match for
 			your specific case.
 		"""
-		if type not in error_types.all_types:
-			type = error_types.UNKNOWN
+		type = BackupErrorType.get(type)  # type: BackupErrorType
 		if errno is None or errno not in _errno_all:
-			errno = error_types.type_errno(type)
+			errno = error_type_errno(type)
 		super(BackupError, self).__init__(errno, message, path)
 		self.__type = type
 
 	@property
 	def type(self):
-		"""The sub-type of an error. See `error_types` submodule."""
+		"""The sub-type of an error. See ``BackupErrorType`` Enum."""
 		return self.__type
 
 
