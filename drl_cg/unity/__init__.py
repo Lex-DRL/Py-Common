59596a257d924a51d8ef79fd0e326750feca0462
Lex Darlog (DRL) <drl.i3x@gmail.com>
2020-04-07 17:58:27 +0300

    drl_cg package + mock sub-packages

---
+++ drl_cg/unity/__init__.py
@@ -0,0 +1,22 @@
+"""
+Tools related to Unity engine.
+"""
+
+__author__ = 'Lex Darlog (DRL)'
+
+# region the regular Type-Hints stuff
+
+try:
+	# support type hints in Python 3:
+	# noinspection PyUnresolvedReferences
+	import typing as _t
+except ImportError:
+	pass
+
+from drl_common.py_2_3 import (
+	str_t as _str_t,
+	str_hint as _str_h
+)
+
+# endregion
+
