7119ffb2301b2ed8065098f814606211c6b14f17
Lex Darlog (DRL) <drl.i3x@gmail.com>
2020-04-08 02:21:43 +0300

    drl_cg.env._clean()

--- drl_cg/env.py
+++ drl_cg/env.py
@@ -20,9 +20,73 @@ from drl_common.py_2_3 import (
 # endregion
 
 import os as _os
+from os import path as _pth
+import errno as _errno
 
 
-DRL_CG_PATH = _os.environ.get(
-	'DRL_CG_PATH',
-	'C:/1-CG'
-).replace('\\', '/').rstrip('/')
+def _clean(
+	path,  # type: _str_h
+	is_file=False,
+	is_dir=False,
+	exists=False,
+	raise_error=False,
+):
+	"""
+	Clean-up a path:
+
+		* make sure path has unix-style slashes
+		* without any triling slashes
+		* (optionally) make sure it actually exists as a file/dir/whatever
+
+	:param path: the actual path
+	:param is_file: make sure the path exists and points to a file
+	:param is_dir: make sure the path exists and points to a directory
+	:param exists: make sure the path exists
+	:param raise_error:
+		What to do if any of above checks is active and path doesn't exist:
+			* `True` - raise a corresponding `OSError`.
+			* `False` - just return an empty string.
+	:return:
+	"""
+	if isinstance(path, _str_t):
+		path = path.replace('\\', '/').rstrip('/')
+	else:
+		path = ''
+
+	if not path:
+		if raise_error:
+			errno = _errno.ENOENT
+			raise OSError(errno, _os.strerror(errno), path)
+		return ''
+
+	if is_file:
+		if not _pth.isfile(path):
+			if raise_error:
+				errno = _errno.EISDIR if _pth.exists(path) else _errno.ENOENT
+				raise OSError(errno, _os.strerror(errno), path)
+			return ''
+		return path
+
+	if is_dir:
+		if not _pth.isdir(path):
+			if raise_error:
+				errno = _errno.ENOTDIR if _pth.exists(path) else _errno.ENOENT
+				raise OSError(errno, _os.strerror(errno), path)
+			return ''
+		return path
+
+	if exists:
+		if not _pth.exists(path):
+			if raise_error:
+				errno = _errno.ENOENT
+				raise OSError(errno, _os.strerror(errno), path)
+			return ''
+		return path
+
+	return path
+
+
+DRL_CG_PATH = _clean(
+	_os.environ.get('DRL_CG_PATH', 'C:/1-CG'),
+	is_dir=True
+)
